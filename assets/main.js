abdc26f53b6383a88ac36c420a2d6681be60ca04
Mustafa Berk Kalkan <berkklkan@gmail.com>
2025-01-11 12:39:16 +0300

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+const modal = document.querySelector(".thx-modal");
+let deneme = null;
+
+
+btnSendMail.addEventListener("click", () => {
+    container.style.display = "none";
+    modal.style.display = "flex";
+    
+})
+
+btnDismiss.addEventListener("click", () => {
+    modal.style.display = "none";
+    container.style.display = "flex";
+})
